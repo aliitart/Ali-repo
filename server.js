4fce602914646a0ef9daf165ee87a62becc9f4f7
aliitart <paredesalmeida-a@webmail.uwinnipeg.ca>
2026-09-24 17:57:31 -0500

    Update welcome message

--- server.js
+++ server.js
@@ -8,11 +8,12 @@ app.get("/", (req, res) => {
     res.send(`
         <!DOCTYPE html>
         <html>
+        <link rel="stylesheet" href="style.css">
         <head>
             <title>Course Server</title>
         </head>
         <body>
-            <h1>Hello from Ali's server!</h1>
+            <h1>Hello from Alison's server!</h1>
             <p>This page is being served by Node.js and Express.</p>
         </body>
         </html>
@@ -21,4 +22,25 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server listening on port ${PORT}`);
+});
+
+app.get('/api/getName', (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.json({ name: 'Alison\'s site' });
+});
+
+/*
+res.send
+res.json
+{name: '___"}
+/*
+
+git add .
+git commit -m "__"
+git push
+*/
+
+app.get('api/getImage', (req, res) => {
+    res.set('Access-Control-ALLOW-origin', '*');
+    res.sendFile('path/to/your/image.png');
 });
