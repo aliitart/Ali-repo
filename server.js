75750d2e3edd0fd1ae11ae409deb0a75309d723c
aliitart <paredesalmeida-a@webmail.uwinnipeg.ca>
2026-09-24 18:08:13 -0500

    Update getImage

--- server.js
+++ server.js
@@ -42,5 +42,5 @@ git push
 
 app.get('api/getImage', (req, res) => {
     res.set('Access-Control-ALLOW-origin', '*');
-    res.sendFile('path/to/your/image.png');
+    res.sendFile('../Adv. Internet Programming/serverProfile.png');
 });
