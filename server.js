eb8ecb293ea21271c18ea3654483d5f66af3ef7a
aliitart <paredesalmeida-a@webmail.uwinnipeg.ca>
2026-09-24 22:10:51 -0500

    update getImage

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/api/getName', (req, res) => {
 
 app.get('/api/getImage', (req, res) => {
     res.set('Access-Control-Allow-Origin', '*');
-    res.sendFile(path.join(__dirname, "image.jpeg"));
+    res.sendFile(path.join(__dirname, "getImage.jpeg"));
 });
 
 /*
